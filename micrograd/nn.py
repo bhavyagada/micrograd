bc83c7291ebaaefb840902bd886b949e8e331485
bhavya <bhavyasgada@gmail.com>
2023-01-16 19:13:29 -0800

    bug fix

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -11,7 +11,7 @@ class Module:
         return []
 
 class Neuron(Module):
-    def __init__(self, nin, nonlin):
+    def __init__(self, nin, nonlin=True):
         self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
         self.b = Value(0)
         self.nonlin = nonlin
@@ -21,7 +21,7 @@ class Neuron(Module):
         act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
         return act.relu() if self.nonlin else act
 
-    def parameteres(self):
+    def parameters(self):
         return self.w + [self.b]
     
     def __repr__(self):
@@ -32,8 +32,8 @@ class Layer(Module):
         self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
 
     def __call__(self, x):
-        outs = [n(x) for n in self.neurons]
-        return outs[0] if len(outs) == 1 else outs
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
 
     def parameters(self):
         return [p for neuron in self.neurons for p in neuron.parameters()]
@@ -44,7 +44,7 @@ class Layer(Module):
 class MLP(Module):
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
 
     def __call__(self, x):
         for layer in self.layers:
